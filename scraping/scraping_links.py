ed042ceb5c041559105cfc1cb49f28de9f57d394
Alymbekov Dastan <dastan.alymbekov@data-ox.com>
2022-02-02 18:11:05 +0600

    add links scraper, add selenium, optimize code

--- scraping/scraping_links.py
+++ scraping/scraping_links.py
@@ -1,73 +1,55 @@
-import asyncio
 import os
 import sys
-from pyppeteer import launch
+import time
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.by import By
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-from scraping import helpers
+
+from scraping.base_scraping import load_page
+from scraping.helpers import wait_for_xpath
+
 
 data_links = []
 
 
-async def get_link_from_html(data):
+def load_page_data(driver):
+    wait_for_xpath(driver, '/html/body/div/div/div/div[4]/div/div/button[4]')
+    result = get_link_from_html(driver.page_source)
+    for res in result:
+        data_links.append(res)
+
+
+def get_link_from_html(data):
     soup = BeautifulSoup(data, 'html.parser')
-    links = soup.find_all('a')
+    divs = soup.find_all('div', class_='blueprint-thumbnail')
+    links = []
+    for div in divs:
+        links.append("https://factorioprints.com" + str(div.find('a').get("href")))
     return links
 
 
-async def click_next_btn_by_xpath(page):
-    elements = await page.xpath('/html/body/div/div/div/div[4]/div/div/button[4]')
-    return elements[0]
+def click_next_btn_by_xpath(driver):
+    element = driver.find_element(By.XPATH, '/html/body/div/div/div/div[4]/div/div/button[4]')
+    element.click()
+    return driver
 
 
-async def load_page(url):
-    browser = await launch(headless=True)
-    page = await browser.newPage()
-    await page.setUserAgent(helpers.get_usage())
-    await page.goto(url)
-    return page
+def pagination(driver):
+    click_next_btn_by_xpath(driver)
+    return driver
 
 
-async def load_page_data(page):
-    try:
-        import random
-        await page.waitForXPath('/html/body/div/div/div/div[4]/div/div/button[4]')
-        await page.screenshot({'path': f'example{random.randint(666, 999)}.png'})
-        data = await page.evaluate("""
-            document.querySelector('*').outerHTML
-        """)
-        links = await get_link_from_html(data)
-        data_links.append(links)
-    except:
-        pass
-
-
-async def pagination(page):
-    try:
-        next_btn = await click_next_btn_by_xpath(page)
-        await next_btn.click()
-        await page.waitFor(5000)
-        return page
-    except Exception as e:
-        raise ValueError("Пагинация не сработало")
-
-
-async def main_func():
-    url = "https://factorioprints.com/top"
-    page = await load_page(url)
+def main(url):
+    driver = load_page(url)
     for n in range(0, 10):
-        await load_page_data(page)
-        page = await pagination(page)
-    await page.close()
+        load_page_data(driver)
+        driver = pagination(driver)
 
-event_loop = asyncio.get_event_loop()
-try:
-    event_loop.run_until_complete(main_func())
-except Exception as e:
-    print(data_links)
-finally:
-    event_loop.close()
+    return data_links
+
+
+if __name__ == '__main__':
+    links = main("https://factorioprints.com/top")
+    print(links)
+    print(len(links))
