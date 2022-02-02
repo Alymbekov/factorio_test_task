cf50a78b6f89626e3689e33a8260a4abc6b1cd85
Alymbekov Dastan <dastan.alymbekov@data-ox.com>
2022-02-02 20:22:08 +0600

    add full details scraper, add download image functionality

--- scraping/helpers.py
+++ scraping/helpers.py
@@ -1,7 +1,18 @@
+import os
 import random
+import sys
+from pathlib import Path
+from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+
+def init_bs4(html):
+    return BeautifulSoup(html, 'html.parser')
 
 
 def get_usage():
@@ -14,3 +25,29 @@ def wait_for_xpath(driver, xpath):
     wait = WebDriverWait(driver, 5)
     wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
 
+
+def click_btn_by_xpath(xpath, driver):
+    element = driver.find_element(By.XPATH, xpath)
+    element.click()
+    return driver
+
+
+def get_parent_directory():
+    path = Path(os.path.abspath(__file__))
+    directory = Path(path.parent.absolute()).parent
+    return directory
+
+
+def generate_file_name(file_name):
+    return file_name.replace('/', '')
+
+
+def download_and_save_image(driver, xpath):
+    html = driver.page_source
+    soup = init_bs4(html)
+    src_img = soup.find('img').get('src')
+    file_name = generate_file_name(src_img)
+    fn = os.path.join(get_parent_directory()) + f'/media/images/{file_name}.png'
+    with open(fn, 'wb') as file:
+        file.write(driver.find_element(By.XPATH, xpath).screenshot_as_png)
+    return fn
--- scraping/scraping_links.py
+++ scraping/scraping_links.py
@@ -1,14 +1,13 @@
 import os
 import sys
-import time
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
+
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
+from scraping.scraping_details import run_scrape_details
 from scraping.base_scraping import load_page
-from scraping.helpers import wait_for_xpath
-
+from scraping.helpers import wait_for_xpath, click_btn_by_xpath
 
 data_links = []
 
@@ -20,8 +19,8 @@ def load_page_data(driver):
         data_links.append(res)
 
 
-def get_link_from_html(data):
-    soup = BeautifulSoup(data, 'html.parser')
+def get_link_from_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
     divs = soup.find_all('div', class_='blueprint-thumbnail')
     links = []
     for div in divs:
@@ -29,18 +28,12 @@ def get_link_from_html(data):
     return links
 
 
-def click_next_btn_by_xpath(driver):
-    element = driver.find_element(By.XPATH, '/html/body/div/div/div/div[4]/div/div/button[4]')
-    element.click()
-    return driver
-
-
 def pagination(driver):
-    click_next_btn_by_xpath(driver)
+    click_btn_by_xpath(driver=driver, xpath='/html/body/div/div/div/div[4]/div/div/button[4]')
     return driver
 
 
-def main(url):
+def run_scrape_links(url):
     driver = load_page(url)
     for n in range(0, 10):
         load_page_data(driver)
@@ -50,6 +43,6 @@ def main(url):
 
 
 if __name__ == '__main__':
-    links = main("https://factorioprints.com/top")
-    print(links)
-    print(len(links))
+    links = run_scrape_links("https://factorioprints.com/top")
+    for link in links[0:2]:
+        run_scrape_details(link)
